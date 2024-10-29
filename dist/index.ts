8735a14857677f049d1d5e617ac2767a84d10aec
studpod <podobin455467@gmail.com>
2024-10-29 17:04:28 +0200

    compilation of all .ts files

---
+++ dist/index.ts
@@ -0,0 +1,10 @@
+
+const greeting: string = "Hello, TypeScript!";
+
+console.log(greeting);
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+console.log("Result of add function:", add(5, 3));
---
+++ web/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const greeting = "Hello, TypeScript!";
+console.log(greeting);
+function add(a, b) {
+    return a + b;
+}
+console.log("Result of add function:", add(5, 3));
