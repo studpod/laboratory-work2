02f174e3de79577bbb6957088395d1ac92fd7398
studpod <podobin455467@gmail.com>
2024-12-05 00:09:35 +0200

    add Webpack and Webpack CLI

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',  
+  output: {
+    filename: 'main.js',  
+    path: path.resolve(__dirname, 'dist'),  
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,  
+        use: ['style-loader', 'css-loader'],  
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',  
+    }),
+  ],
+  devServer: {
+    static: './dist',  
+    open: true, 
+  },
+  mode: 'development',  
+};
